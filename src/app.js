158a16cfd9152efb542e406da27b60cd74aa7ff1
MK2018 <MK2018@users.noreply.github.com>
2015-09-30 10:12:27 -0400

    Added support for schedule displaying
    First step towards proper implementation of TJ Schedule

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ var window = new UI.Window({
 // Create a background Rect
 var bgRect = new UI.Rect({
   position: new Vector2(0, 0),
-  size: new Vector2(144, 120),
+  size: new Vector2(144, 80),
   backgroundColor: 'white'
 });
 window.add(bgRect);
@@ -39,13 +39,34 @@ var timeText = new UI.TimeText({
   textAlign: 'center'
 });
 
-var timeLeft = new UI.TimeText({
-  position: new Vector2(0, 130),
+var timeLeft = new UI.Text({
+  position: new Vector2(0, 112),
   size: new Vector2(144, 30),
-  text: "Time left: " + parseInt(date),
+  text: "Loading...",
+  font: 'gothic-24',
   color: 'white',
   textAlign: 'center',
 });
+
+var weather = new UI.Text({
+  position: new Vector2(100, 75),
+  size: new Vector2(44, 30),
+  text: "32.0",
+  font: 'gothic-24',
+  color: 'white',
+  textAlign: 'center',
+});
+
+var back_image = new UI.Image({
+  position: new Vector2(0, 0),
+  size: new Vector2(144, 168),
+  backgroundColor: 'clear',
+  image: 'images/tjTimeBack.png',
+});
+
+window.add(back_image);
+
+window.add(weather);
 
 window.add(timeLeft);
 
@@ -54,6 +75,25 @@ window.add(timeText);
 
 // Show the Window
 window.show();
+
+function getMinSinceMidnight(d) {
+  var e = new Date(d);
+  return ((d - e.setHours(0,0,0,0))/1000)/60;
+}
+
+function analyzeTime(jsonObj){
+  for(var i = 1; i <= parseInt(jsonObj[0]); i++){
+    console.log("Current time: " + parseInt(getMinSinceMidnight(new Date())));
+    console.log("start time: " + parseInt(JSON.stringify(jsonObj[2].startInt)));
+    if(parseInt(JSON.stringify(jsonObj[i].endInt))>parseInt(getMinSinceMidnight(new Date()))&&parseInt(JSON.stringify(jsonObj[i].startInt))<parseInt(getMinSinceMidnight(new Date()))){
+      //console.log(JSON.stringify(jsonObj[i].name)+" is in progess");
+      return JSON.stringify(jsonObj[i].name)+"\n"+JSON.stringify(jsonObj[i].startForm)+ " - " +JSON.stringify(jsonObj[i].endForm);
+    }     
+  }
+  return "No class right now. Enjoy!";
+}
+
+//console.log(getMinSinceMidnight(new Date()));
 
 var xhrRequest = function (url, type, callback) {
   var xhr = new XMLHttpRequest();
@@ -64,15 +104,22 @@ var xhrRequest = function (url, type, callback) {
   xhr.send();
 };
 
+
+
 function fetchSchedule(){
   var url = "http://www.tjtimelib.appspot.com";
   xhrRequest(url, 'GET', 
     function(responseText) {
       // responseText contains a JSON object with weather info
       var jsonObj = JSON.parse(responseText);
-      var jsonTxt = JSON.stringify(jsonObj);
+      /*var jsonTxt = JSON.stringify(jsonObj);
       console.log(jsonTxt);
-      console.log(JSON.stringify(jsonObj[0].start));
+      console.log("array length" + jsonObj.length);
+      console.log(JSON.stringify(jsonObj[0].start));*/
+      var timeAnalysis = analyzeTime(jsonObj);
+      //var toPrint = JSON.stringify(jsonObj[1].name)+"\n"+JSON.stringify(jsonObj[1].startForm)+ " - " +JSON.stringify(jsonObj[1].endForm);
+      //toPrint = toPrint.replace(/\"/g, "");
+      timeLeft.text(timeAnalysis.replace(/\"/g, ""));
     }      
   );
 }
@@ -80,8 +127,9 @@ function fetchSchedule(){
 fetchSchedule();
 
 function start(){
+  fetchSchedule();
   date = (parseInt(nextTime - (Date.now()/1000))/60)+1;
-  timeLeft.text(parseInt(date) + " min left");
+  //timeLeft.text(parseInt(date) + " min left");
   window.add(timeLeft);
   window.show();
   setTimeout(start, 5000);
